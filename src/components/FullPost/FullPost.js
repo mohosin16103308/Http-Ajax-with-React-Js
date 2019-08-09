32e479fd016446efe84c3ec108be642e2db742c6
mohosin16103308 <hamza1610330816@gmail.com>
2019-08-10 02:56:12 +0600

    Fetching Single Post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+state = {
+    loadedPost:null
+}
+
+componentDidUpdate(){
+   
+    axios.get(' https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+    .then(response => 
+      
+      {
+    
+          if(!this.state.loadedPost ){
+            this.setState({loadedPost:response.data});
+          } 
+      }
+);
+    }
+
     render () {
         let post = <p>Please select a Post!</p>;
         if(this.props.id)
-     {   post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
+     {   
+  if(this.state.loadedPost){
+    post = (
+        <div className="FullPost">
+            <h1>{this.state.loadedPost.title}</h1>
+            <p>{this.state.loadedPost.body}</p>
+            <div className="Edit">
+                <button className="Delete">Delete</button>
             </div>
+        </div>
 
-        );}
+    );
+  }
+
+     }
         return post;
     }
 }
